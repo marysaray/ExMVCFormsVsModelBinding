1aad9a49fd4ef5139522435c2b2b90b049fa8fc0
Mary Saray <msaray.ms@outlook.com>
2021-07-22 17:48:17 -0700

    Created add view

---
+++ ExMVCFormsVsModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExMVCFormsVsModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Add student to database
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
